9e57a35148a76b27b3a137e8a4c9230532a77e2b
Sebastian Guesdon <sebastian.guesdon@nrco.fr>
2020-06-02 13:36:56 +0200

    hotfix strPrefix et déclaration de macro

--- src/DOM/NodeTrait.php
+++ src/DOM/NodeTrait.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright (c)2014-2014 heiglandreas
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -11,7 +11,7 @@
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * @category 
+ * @category
  * @author    Andreas Heigl<andreas@heigl.org>
  * @copyright ©2018-2018 NR-Communication
  * @license   http://www.opesource.org/licenses/mit-license.php MIT-License
@@ -53,17 +53,18 @@ trait NodeTrait {
         if($this->parentNode->lastChild === $this) {
             $strPostfix = '';
         }
-        
+
         $macroTag = '';
-        if($this->getAttribute('macro')) {
+        $macroName = $this->getAttribute('macro');
+        $macroParentName = $this->getParentNameMacro();
+
+        if($macroName && $macroName !== $macroParentName) {
             $macroTag = '<&' . $this->getAttribute('macro') . '>';
         }
-        
-        $prevMacroTag = '';
-        $prevMacroName = $this->getParentNameMacro();
 
-        if($prevMacroName) {
-            $prevMacroTag = '<&' . $prevMacroName . '>';
+        $prevMacroTag = '';
+        if($macroParentName && $macroParentName !== $macroName) {
+            $prevMacroTag = '<&' . $macroParentName . '>';
         }
 
         foreach ($this->childNodes as $child) {
@@ -74,17 +75,17 @@ trait NodeTrait {
             }
         }
 
-        return $strPrefix
-             . $macroTag
-             . $content
-             . $prevMacroTag
-             . $strPostfix;
+        return $macroTag
+            . $strPrefix
+            . $content
+            . $strPostfix
+            . $prevMacroTag;
     }
 
     private function getParentNameMacro($node = false) {
-        
+
         $macro = false;
-        
+
         if($node === false) {
             $node = $this->parentNode;
         }
